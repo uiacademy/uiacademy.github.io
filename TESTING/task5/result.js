d46dcb63927634e53964c5a7befc1725fb1289df
Darius Aseriskis <daseriskis@kayak.com>
2015-01-22 09:08:49 +0200

    Adding lecture list and slide changes

--- TESTING/task5/result.js
+++ TESTING/task5/result.js
@@ -1,30 +1,23 @@
 
-describe("A suite", function() {
-   it("contains spec with an expectation", function() {
-      expect(true).toBe(true);
+describe("MVC test", function() {
+  
+   it("Model test", function() {
+       // nothing to test ?
    });
-   
-   it("Must check for parameter count", function() {
-       try {
-            sumIf();   
-            expect.fail();
-       }
-       catch(e) {
-       }
-       try {
-            sumIf(null, null, null);
-            expect.fail();
-       }
-       catch(e) {
-       }
-      
+    
+   it("View test", function() {
+      var View = new SimpleForm.View();
+       
+      // init - callback test
+       
+      // format message test
+       
+      // update message test
+       
+      // show message test
    });
-   
-   it("Check default behavior works", function() {
-      expect(sumIf([3,3,4])).toBe(10);
-   });
-   
-   it("Check custom filtering behavior works", function() {
-      expect(sumIf([3,3,4], function(a) { return a != 4 ? a : 1 })).toBe(7);
+    
+   it("Controller test", function() {
+       // nothing to test      
    });
 }); 
