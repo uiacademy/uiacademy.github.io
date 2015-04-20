101d51e40971da104fab3526fb44d05606eda686
Darius Aseriskis <daseriskis@kayak.com>
2015-04-20 15:54:39 +0300

    adding sample tests

---
+++ studapp/web/unit-tests/hello.test.js
@@ -0,0 +1,11 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(false);
+  });
+});
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
